2c4cff285ccd23d2a68bcfee51b52a60af5a6523
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-02 22:02:41 +0700

    Adding Material UI

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,31 +1,15 @@
-import "./IndexPage.css";
+import './IndexPage.css';
 
-import type { FC } from "react";
+import type { FC } from 'react';
 
 import { Link } from "../../components/Link";
 import { Page } from "../../components/Page";
 import { routes } from "../../navigation/routes.ts";
 import { useNavigate } from "react-router-dom";
-import { Button, TextField } from "@mui/material";
 
 export const IndexPage: FC = () => {
-  const data = JSON.stringify({
-    eventType: 'web_app_setup_closing_behavior',
-    eventData: {
-      is_visible: true,
-    },
-  });
-
-  window.parent.postMessage(data, 'https://web.telegram.org');
-
   return (
     <Page title="Home Page">
-      <div>
-        <Button variant="contained">Getting Started</Button>
-      </div>
-      <div>
-        <TextField required id="outlined-basic" label="Full Name" variant="outlined" />
-      </div>
       <p>
         This page is a home page in this boilerplate. You can use the links
         below to visit other pages with their own functionality.
---
+++ src/pages/QuickScanPage/QuickScanPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+export const QuickScanPage: FC = () => {
+  return (
+    <div>
+      <h1>Quick Scan</h1>
+    </div>
+  );
+};
