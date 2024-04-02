aeddef261297b779cdf34e03136641321c485c1a
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-02 17:08:49 +0700

    add input testing

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,24 +1,31 @@
-import './IndexPage.css';
+import "./IndexPage.css";
 
-import type { FC } from 'react';
+import type { FC } from "react";
 
-import { Link } from '../../components/Link';
-import { Page } from '../../components/Page';
-import { routes } from '../../navigation/routes.ts';
+import { Link } from "../../components/Link";
+import { Page } from "../../components/Page";
+import { routes } from "../../navigation/routes.ts";
 
 export const IndexPage: FC = () => {
   return (
     <Page title="Home Page">
       <p>
-        This page is a home page in this boilerplate. You can use the links below to visit other
-        pages with their own functionality.
+        This page is a home page in this boilerplate. You can use the links
+        below to visit other pages with their own functionality.
       </p>
+      <div>
+        <input type="text" placeholder="Enter your full name" />
+        <input type="text" placeholder="Enter your dob" />
+      </div>
       <ul className="index-page__links">
-        {routes.map(({ path, title }) => title && (
-          <li className="index-page__link" key={path}>
-            <Link to={path}>{title}</Link>
-          </li>
-        ))}
+        {routes.map(
+          ({ path, title }) =>
+            title && (
+              <li className="index-page__link" key={path}>
+                <Link to={path}>{title}</Link>
+              </li>
+            ),
+        )}
       </ul>
     </Page>
   );
