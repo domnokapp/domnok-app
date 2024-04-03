06c715141e5fc263bbd8962ae5aa1c0c6ed41841
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-03 11:32:26 +0700

    Testing closing app and hide setting

--- src/navigation/routes.ts
+++ src/navigation/routes.ts
@@ -3,6 +3,7 @@ import type { ComponentType } from 'react';
 import { IndexPage } from '../pages/IndexPage';
 import { InitDataPage } from '../pages/InitDataPage';
 import { LaunchParamsPage } from '../pages/LaunchParamsPage';
+import { ProductFormPage } from '../pages/ProductPage';
 import { QuickScanPage } from '../pages/QuickScanPage';
 import { ScanQrPopup } from '../pages/ScanQrPopup';
 import { ThemeParamsPage } from '../pages/ThemeParamsPage';
@@ -26,4 +27,5 @@ export const routes: Route[] = [
   { path: '/ton-connect', Component: TONConnectPage, title: 'TON Connect' },
   { path: '/quick-scan', Component: QuickScanPage, title: 'Quick Scan' },
   { path: '/scan-qr-popup', Component: ScanQrPopup, title: 'Scan QR Popup' },
+  { path: '/manage-product', Component: ProductFormPage, title: 'Manage Product' },
 ];
