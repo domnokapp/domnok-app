caf3c1fe50a98fae878b5945eb01afaf27b05d81
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-03 09:12:48 +0700

    Testing main BTN

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,10 +1,10 @@
-import { setDebug, postEvent } from "@tma.js/sdk";
-import { DisplayGate, SDKProvider } from "@tma.js/sdk-react";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import type { FC } from "react";
-import { useEffect, useMemo } from "react";
+import { setDebug, init } from '@tma.js/sdk';
+import { DisplayGate, SDKProvider } from '@tma.js/sdk-react';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import type { FC } from 'react';
+import { useEffect, useMemo } from 'react';
 
-import { App } from "../App";
+import { App } from '../App';
 
 const Err: FC<{ error: unknown }> = ({ error }) => {
   return (
@@ -25,8 +25,18 @@ const Loading: FC = () => {
 
 export const Root: FC = () => {
   const manifestUrl = useMemo(() => {
-    return new URL("tonconnect-manifest.json", window.location.href).toString();
+    return new URL('tonconnect-manifest.json', window.location.href).toString();
   }, []);
+
+  const { mainButton, viewport } = init();
+  mainButton.on('click', () => viewport.expand());
+
+  mainButton
+    .setBackgroundColor('#ff0000')
+    .setTextColor('#ffffff')
+    .setText('Expand')
+    .enable()
+    .show();
 
   // Enable debug mode to see all the methods sent and events received.
   useEffect(() => {
