2d97bbc39ce56379e537fad67d5aac3bd3a2e818
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-04 15:58:07 +0700

    add gestalt components package

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AppRoot } from '@xelene/tgui';
 
 import { Root } from './components/Root';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <AppRoot>
     <Root />
-  </React.StrictMode>,
+  </AppRoot>,
+  document.getElementById('root')
 );
