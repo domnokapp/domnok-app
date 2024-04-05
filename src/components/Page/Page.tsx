04952480de974d32dc5c32797b22857f4d32ac5d
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-05 15:32:48 +0700

    get User info

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -5,7 +5,7 @@ import type { FC, PropsWithChildren } from 'react';
 export const Page: FC<PropsWithChildren<{ title: string }>> = ({ title, children }) => {
   return (
     <div className="page">
-      <h1 className="page__title">{title}</h1>
+      {/* <h1 className="page__title">{title}</h1> */}
       {children}
     </div>
   );
