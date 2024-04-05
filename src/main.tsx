7e837e158856f7d3c0d5ddd224361eec78eb2999
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-05 10:25:13 +0700

    Added mantine component packages

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import '@xelene/tgui/dist/styles.css';
 // core styles are required for all packages
 import '@mantine/core/styles.css';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRoot } from '@xelene/tgui';
 
