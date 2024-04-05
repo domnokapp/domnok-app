1bf94ac6a3106cb34b32765297750f532de4b53e
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-05 10:06:45 +0700

    Added mantine component packages

--- src/components/Cards/SetupTeam.tsx
+++ src/components/Cards/SetupTeam.tsx
@@ -2,7 +2,7 @@ import { Button, Paper, Text, Group, CloseButton } from '@mantine/core';
 
 export function SetupTeam() {
     return (
-        <Paper withBorder p="lg" radius="md" shadow="md" m="sm" variant="outline">
+        <Paper withBorder p="lg" radius="md" shadow="md" mr="sm" mt="sm" ml="sm" mb={0} variant="outline">
             <Group justify="space-between" mb="xs">
                 <Text fw={500} size="lg">
                     បង្កើតហាងថ្មី
