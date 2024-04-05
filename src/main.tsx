0559ffa00996e6f443996476543e68c48c3e9137
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-05 08:30:44 +0700

    Added mantine component packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import './index.css';
 import '@xelene/tgui/dist/styles.css';
+// core styles are required for all packages
+import '@mantine/core/styles.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRoot } from '@xelene/tgui';
 
 import { Root } from './components/Root';
+import { createTheme, MantineProvider } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <AppRoot>
-    <Root />
+    <MantineProvider theme={theme}>
+      <Root />
+    </MantineProvider>
   </AppRoot>,
   document.getElementById('root')
 );
