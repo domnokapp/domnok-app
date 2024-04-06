68b6a857dd476820675f3048fc1468bfaee6facd
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-06 21:50:08 +0700

    added user banner information

---
+++ src/components/Cards/BannerInformationCard.tsx
@@ -0,0 +1,36 @@
+import { UnstyledButton, Group, Avatar, Text, rem } from '@mantine/core';
+import { IconChevronRight } from '@tabler/icons-react';
+import classes from './UserButton.module.css';
+import { Link } from '../Link';
+
+type BannerInformationProps = {
+    id: number | undefined;
+    code: string | undefined;
+    name: string | undefined;
+    teamName: string | undefined;
+    photoUrl: string | undefined;
+};
+
+export function BannerInformationCard({code, name, photoUrl, teamName}: BannerInformationProps) {
+  return (
+    <UnstyledButton className={classes.user}>
+        <Link to="/manage-product">
+            <Group>
+                <Avatar
+                    src={ photoUrl != null ? photoUrl : "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png" }
+                    radius="xl"
+                />
+                <div style={{ flex: 1 }}>
+                    <Text size="sm" fw={500}>
+                        {teamName}
+                    </Text>
+                    <Text c="dimmed" size="xs">
+                        #{name}[{code}]
+                    </Text>
+                </div>
+                <IconChevronRight size="md" stroke={1.5} />
+            </Group>
+        </Link>
+    </UnstyledButton>
+  );
+}
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -8,6 +8,7 @@ import { useInitData } from '@tma.js/sdk-react';
 import { UserCard } from '../../components/Cards/UserCard.tsx';
 import { SetupTeam } from '../../components/Cards/SetupTeam.tsx';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BannerInformationCard } from '../../components/Cards/BannerInformationCard.tsx';
 const BASE_URL = "https://app.domnok.com/api/v1";
 const ROUT_CONNECT_TELEGRAM_API = "/user/connect-tg-id";
 
@@ -36,50 +37,56 @@ async function connectAPI(params: any) {
 }
 
 export const IndexPage: FC = () => {
-  
-  const initData = useInitData();
-  const userObj = useMemo<User | undefined>(() => {
+    const initData = useInitData();
+    const userObj = useMemo<User | undefined>(() => {
 
-    if (!initData) {
-      return;
-    }
-
-    const { user } = initData;
-    return user;
-
-  }, [initData]);
-
-  const apiUser = useMemo(async() => {
-      const user = await AsyncStorage.getItem("UserLogin");
-
-      if ( user != null ) {
-        return JSON.parse(user);
+      if (!initData) {
+        return;
       }
-  }, []);
 
-  console.log("User", apiUser);
+      const { user } = initData;
+      return user;
+    }, [initData]);
+
+    const apiUser = useMemo(async() => {
+        const user = await AsyncStorage.getItem("UserLogin");
+        if ( user != null ) {
+          return JSON.parse(user).data;
+        }
+    }, []);
 
     useEffect(() => {
-      connectAPI({
-        name: userObj?.firstName,
-        email: null,
-        phone: null,
-        tg_connect_id: userObj?.id,
-      });
+      if (apiUser != null) {
+        connectAPI({
+          name: `${userObj?.lastName} ${userObj?.firstName}`,
+          email: null,
+          phone: null,
+          tg_connect_id: userObj?.id,
+        });
+      }
     }, []);
+
+    console.log("User", apiUser?.user);
 
   return (
     <Page title="Dashboard">
-      { userObj
+      { apiUser
           ? (
             <>
-              <UserCard
+              {/* <UserCard
                 id={userObj.id}
                 firstName={userObj.firstName}
                 lastName={userObj.lastName}
                 photoUrl={userObj.photoUrl} 
                 username={userObj.username}
                 phoneNumber=""
+              /> */}
+              <BannerInformationCard
+                id={apiUser?.user?.id}
+                code={apiUser?.user?.code}
+                name={apiUser?.user?.name}
+                teamName={apiUser?.user?.name}
+                photoUrl={apiUser?.user?.name}
               />
               <SetupTeam />
               <ActionsGrid />
