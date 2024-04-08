a0aced2c5d0d908a4931311438fac9725c43334f
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-08 15:45:08 +0700

    added user banner information

--- src/pages/UserPage/ProfilePage.tsx
+++ src/pages/UserPage/ProfilePage.tsx
@@ -1,11 +1,34 @@
 import type { FC } from 'react';
 import { Typography } from 'antd';
+import classes from './UserPage.module.css';
+import { useState } from 'react';
+import { TextInput } from '@mantine/core';
+import { Page } from '../../components/Page';
 
 export const ProfilePage: FC = () => {
+  const [focused, setFocused] = useState(false);
+  const [name, setName] = useState('');
+  const floating = name.trim().length !== 0 || focused || undefined;
 
   return (
     <>
+      <Page title='Profile Page'>
         <Typography.Title level={3}>Profile Page</Typography.Title>
+        <TextInput
+          label="គោត្តនាមនិងនាម"
+          placeholder="បញ្ចូលឈ្មោះពេញរបស់អ្នក"
+          required
+          classNames={classes}
+          value={name}
+          onChange={(event) => setName(event.currentTarget.value)}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          mt="md"
+          autoComplete="nope"
+          data-floating={floating}
+          labelProps={{ 'data-floating': floating }}
+        />
+      </Page>
     </>
   );
 };
