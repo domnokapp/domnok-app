cb52a282e7ff1ea9b83c9b3fe65a09ef8930fbb2
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-09 09:18:14 +0700

    added user banner information

--- src/pages/UserPage/ProfilePage.tsx
+++ src/pages/UserPage/ProfilePage.tsx
@@ -4,32 +4,52 @@ import type { FC } from 'react';
 import { Typography } from 'antd';
 import classes from './UserPage.module.css';
 import { useState } from 'react';
-import { TextInput } from '@mantine/core';
+import { TextInput, PasswordInput, Tooltip, Center, Text, rem } from '@mantine/core';
 import { Page } from '../../components/Page';
+import { IconInfoCircle } from '@tabler/icons-react';
 
 export const ProfilePage: FC = () => {
-  const [focused, setFocused] = useState(false);
   const [name, setName] = useState('');
-  const floating = name.trim().length !== 0 || focused || undefined;
+  const [categoryId, setCategoryId] = useState('');
+
+  const rightSection = (
+    <Tooltip
+      label="We store your data securely"
+      position="top-end"
+      withArrow
+      transitionProps={{ transition: 'pop-bottom-right' }}
+    >
+      <Text component="div" c="dimmed" style={{ cursor: 'help' }}>
+        <Center>
+          <IconInfoCircle style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
 
   return (
     <>
       <Page title='Profile Page'>
         <Typography.Title level={3}>Profile Page</Typography.Title>
+
         <TextInput
+          rightSection={rightSection}
           label="គោត្តនាមនិងនាម"
           placeholder="បញ្ចូលឈ្មោះពេញរបស់អ្នក"
-          required
-          classNames={classes}
           value={name}
           onChange={(event) => setName(event.currentTarget.value)}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
           mt="md"
-          autoComplete="nope"
-          data-floating={floating}
-          labelProps={{ 'data-floating': floating }}
         />
+
+        <TextInput
+          rightSection={rightSection}
+          label="គោត្តនាមនិងនាម"
+          placeholder="បញ្ចូលឈ្មោះពេញរបស់អ្នក"
+          value={categoryId}
+          onChange={(event) => setCategoryId(event.currentTarget.value)}
+          mt="md"
+        />
+        
       </Page>
     </>
   );
