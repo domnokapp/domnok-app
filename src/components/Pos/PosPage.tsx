445ab5a464eb96d63596bb2cac13653c7bc6e6f5
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-30 16:04:24 +0700

    added POS page section

--- src/components/Pos/PosPage.tsx
+++ src/components/Pos/PosPage.tsx
@@ -5,7 +5,7 @@ import { Link } from '../Link';
 const items = [
     { id: 1, name: 'Apple', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
     { id: 2, name: 'Banana', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
-    { id: 3, name: 'Manger', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
+    { id: 3, name: 'Manger', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' }
 
 ];
 
