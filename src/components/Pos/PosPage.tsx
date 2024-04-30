fcd11ae06e33f83c08057b0685da00c44c079866
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-30 16:01:59 +0700

    added POS page section

--- src/components/Pos/PosPage.tsx
+++ src/components/Pos/PosPage.tsx
@@ -2,33 +2,35 @@ import { Button, Text, Group, Alert, Image, Card, Badge, ScrollArea, Grid } from
 import { IconBuildingStore } from '@tabler/icons-react';
 import { Link } from '../Link';
 
-function ProductItem() {
+const items = [
+    { id: 1, name: 'Apple', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
+    { id: 2, name: 'Banana', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
+    { id: 3, name: 'Manger', price: '$ 12.55', image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png' },
+
+];
+
+function ProductItem(items: any) {
     return (
-        <Grid.Col span={6}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Card.Section>
-          <Image
-            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-            height={160}
-            alt="Norway"
-          />
-        </Card.Section>
-  
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Norway Fjord Adventures</Text>
-          <Badge color="pink">On Sale</Badge>
-        </Group>
-  
-        <Text size="sm" c="dimmed">
-          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-          activities on and around the fjords of Norway
-        </Text>
-  
-        <Button color="blue" fullWidth mt="md" radius="md">
-          Book classic tour now
-        </Button>
-      </Card>
-      </Grid.Col>
+        <>
+        { items.map((item: any) => (
+            <Grid.Col span={6} key={item.id}>
+                <Card shadow="sm" padding="lg" radius="md" withBorder>
+                    <Card.Section>
+                        <Image
+                            src={item.image}
+                            height={80}
+                            alt="Norway"
+                        />
+                    </Card.Section>
+            
+                    <Group justify="space-between" mt="md" mb="xs">
+                        <Text fw={500} lineClamp={2}>{item.name}</Text>
+                        <Badge color="pink">{item.price}</Badge>
+                    </Group>
+                </Card>
+            </Grid.Col>
+        )) }
+        </>
     );
   }
 
@@ -36,7 +38,7 @@ export function PosPage() {
     return (
         <ScrollArea>
             <Grid>
-                <ProductItem />
+                <ProductItem items={items} />
             </Grid>
         </ScrollArea>
     );
