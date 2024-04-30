50b46ceec278109dc5402fdc1bbd27e93218ac64
Theavuth Nhel <theavuthnhel@theavuth-3.local>
2024-04-30 16:43:55 +0700

    added POS page section

--- src/components/Pos/PosPage.tsx
+++ src/components/Pos/PosPage.tsx
@@ -3,10 +3,10 @@ import { IconBuildingStore } from '@tabler/icons-react';
 import { Link } from '../Link';
 
 const items = [
-    { id: 1, name: 'មេជីធម្មជាតិ OF អរម៉ូន', price: '$ 12.55', image: 'http://em-nka.com/products/of-animal.jpg' },
-    { id: 2, name: 'EM ទឹកអង្ករ', price: '$ 12.55', image: 'http://em-nka.com/products/em-rice.jpg' },
-    { id: 3, name: 'ប្រេងលាបសក់', price: '$ 12.55', image: 'http://em-nka.com/products/em-5.jpg' },
-    { id: 3, name: 'មេជីគោក OF ថនិកសត្វ', price: '$ 12.55', image: 'http://em-nka.com/products/of-animal.jpg' }
+    { id: 1, name: 'មេជីធម្មជាតិ OF អរម៉ូន', price: '$ 12.55', image: 'https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/24014310/2023/7/14/e0d643e5-05bc-4249-833b-0ddf80440f851689274793057BULLMERMenBeigePrintedV-NeckPocketsT-shirt1.jpg' },
+    { id: 2, name: 'EM ទឹកអង្ករ', price: '$ 12.55', image: 'https://rukminim2.flixcart.com/image/850/1000/xif0q/t-shirt/j/r/p/xl-lmts003512-lee-original-imagrjgyx5sv654f.jpeg?q=90&crop=false' },
+    { id: 3, name: 'ប្រេងលាបសក់', price: '$ 12.55', image: 'https://m.media-amazon.com/images/I/51ulmT3YUZL._AC_UY1000_.jpg' },
+    { id: 3, name: 'មេជីគោក OF ថនិកសត្វ', price: '$ 12.55', image: 'https://pbx2-pbww-prod-pbww-cdn.getprintbox.com/media/productimage/fe050866-8169-4645-ad76-10437425291c/Blank%20T-Shirt_thumb_300x300?mt=1588770269.372995' }
 ];
 
 function ProductItem() {
